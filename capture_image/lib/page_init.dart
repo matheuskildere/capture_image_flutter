39805e3f701dcb4a80200a3a54a9770bb124fc43
Matheus Kildere <matheuskildery94@gmail.com>
2019-10-11 00:47:42 -0300

    feat: configuration of code body

---
+++ capture_image/lib/page_init.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PageInit extends StatefulWidget {
+  @override
+  _PageInitState createState() => _PageInitState();
+}
+
+class _PageInitState extends State<PageInit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(),
+          Container(),
+        ],
+      ),
+    );
+  }
+}
