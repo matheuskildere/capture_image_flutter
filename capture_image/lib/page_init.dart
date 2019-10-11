104e09494472a514ca8e9cd4673de136896bbe69
Matheus Kildere <matheuskildery94@gmail.com>
2019-10-11 04:32:32 -0300

    feat: buttom and contaner of image

--- capture_image/lib/page_init.dart
+++ capture_image/lib/page_init.dart
@@ -1,20 +1,48 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PageInit extends StatefulWidget {
   @override
   _PageInitState createState() => _PageInitState();
 }
 
+
 class _PageInitState extends State<PageInit> {
   @override
+  File _imageFile;
+
+  Future <Null> _pickImageFromCamera() async {
+    final File imageFile =
+      await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() => this._imageFile = imageFile);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(),
-          Container(),
-        ],
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.camera, color: Colors.black,),
+              iconSize: 50,
+              onPressed: ()async => await _pickImageFromCamera(),  
+            ),
+          ],
+        ),
       ),
-    );
+
+      body:Column(
+        mainAxisAlignment:  MainAxisAlignment.center,
+        children:<Widget>[
+          if (_imageFile != null)...[
+            Center(
+              child: Image.file(_imageFile,width: 300, height: 300)
+            )
+          ]
+        ]
+      ),
+    ); 
   }
 }
